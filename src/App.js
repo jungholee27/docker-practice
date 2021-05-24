a1a68dbf1077e33ced21263ea6249e9ae77109f8
Jung Ho Lee <jungholee@Jungs-MacBook-Pro.local>
2021-05-24 15:59:06 -0400

    retrying deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hi</p>
+        <p>DANK WEBSERVICES M8TYS</p>
         <a
           className="App-link"
           href="https://reactjs.org"
